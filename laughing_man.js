cc40bac1c98dfd5df31050a70a048b80bfe7c250
H. Sanichar <hsanichar2006@gmail.com>
2026-05-09 19:56:09 -0400

    finishing up

--- laughing_man.js
+++ laughing_man.js
@@ -4,18 +4,42 @@ const SPEED = 5;
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    document.querySelector("#square").addEventListener("click", () => {
+    let squareBox = document.querySelector("#square");
+
+    squareBox.addEventListener("click", () => {
         alert("OMG YOU CLICKED ME!");
+    });
+
+    // change background color when mouseover #square
+    squareBox.addEventListener("mouseover", () => {
+        squareBox.style.backgroundColor = "blue";
+    });
+
+    squareBox.addEventListener("mouseout", () => {
+        squareBox.style.backgroundColor = "red";
     });
 
     let box = document.querySelector("#box");
 
+    // create squares
     for (let i = 0; i < SQUARE_COUNT; i++) {
+
         let square = document.createElement("img");
 
+        // laughing man image
         square.src = "laughing_man.jpg";
         square.alt = "Catch the Laughing Man!";
         square.className = "square";
+
+        // change image on mouseover
+        square.addEventListener("mouseover", () => {
+            square.src = "orochimaru_50x50.jpg";
+        });
+
+        // change image back on mouseout
+        square.addEventListener("mouseout", () => {
+            square.src = "laughing_man.jpg";
+        });
 
         box.appendChild(square);
     }
@@ -35,12 +59,30 @@ document.addEventListener("DOMContentLoaded", () => {
 
         setInterval(() => {
 
+            // hit left/right wall
             if (x <= 0 || x >= maxX) {
+
                 dx *= -1;
+
+                // change border color
+                element.style.borderColor =
+                    "rgb(" +
+                    Math.floor(Math.random() * 256) + "," +
+                    Math.floor(Math.random() * 256) + "," +
+                    Math.floor(Math.random() * 256) + ")";
             }
 
+            // hit top/bottom wall
             if (y <= 0 || y >= maxY) {
+
                 dy *= -1;
+
+                // change border color
+                element.style.borderColor =
+                    "rgb(" +
+                    Math.floor(Math.random() * 256) + "," +
+                    Math.floor(Math.random() * 256) + "," +
+                    Math.floor(Math.random() * 256) + ")";
             }
 
             x += dx;
